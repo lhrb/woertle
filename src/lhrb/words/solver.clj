17fdc316e84628b7d505b17918e539ffee92feb5
lukas <lukas.block@pm.me>
2022-02-03 23:28:21 +0100

    add containo

--- src/lhrb/words/solver.clj
+++ src/lhrb/words/solver.clj
@@ -2,7 +2,9 @@
   (:refer-clojure :exclude [==])
   (:require [clojure.core.logic :refer :all]
             [clojure.core.logic.protocols :refer [walk]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :refer [difference]]))
 
 (def alphabet (set (map char (range 97 123))))
 
@@ -45,6 +47,21 @@
    {}
    (group-by count words)))
 
+(defn build-index2
+  "* first mapping is the letter count
+   * second a mapping letter -> all words which contains the letter"
+  [words]
+ (reduce-kv
+  (fn [m k v]
+    (assoc m k
+           (reduce
+            (fn [acc elem]
+              (assoc acc elem
+                     (set (filter #(str/includes? % (str elem)) v))))
+            {} alphabet)))
+  {}
+  (group-by count words)))
+
 (defn ismatch
     [db w pos letter]
     (fn [env]
@@ -54,6 +71,7 @@
            (for [word (get-in db [pos letter])]
              (unify env w word)))
           env))))
+
 
 
 (comment
@@ -69,12 +87,42 @@
   "draco" "maden"
 
   (def db (get (build-index words) 4))
+  (def db2 (get (build-index2 words) 4))
+
+
+  (defn containo [w pos letter]
+    (fn [env]
+      (let [w (walk env w)]
+        (if (lvar? w)
+          (to-stream
+           (for [word (difference
+                       (get-in db2 [letter])
+                       (get-in db [pos letter]))]
+             (unify env w word)))
+          env))))
 
   (run* [w]
      (ismatch db w 0 \a)
      (fresh [w2]
        (ismatch db w2 1 \m)
-       (== w w2)))
+       (fresh [w3]
+         (containo w3 2 \s)
+         (== w w2)
+         (== w w3))))
+
+
+  (run* [w]
+    (containo w 0 \a))
+
+  (difference
+   (get-in db2 [\a])
+   (get-in db [0 \a]))
+
+  (difference #{1 2} #{1 3})
+
+  (group-by set words)
+
+
 
 
   ,)
