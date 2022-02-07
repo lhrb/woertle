0b3b662dd9860dd48c394c6141bd33ebbde1b76f
lukas <lukas.block@pm.me>
2022-02-07 20:53:37 +0100

    unify indexes with protocol

--- src/lhrb/words/solver.clj
+++ src/lhrb/words/solver.clj
@@ -4,27 +4,13 @@
             [clojure.core.logic.protocols :refer [walk]]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.set :refer [difference intersection]]))
+            [clojure.set :refer [difference intersection union]]))
 
 (def alphabet (set (map char (range 97 123))))
 
 (def words
   (with-open [reader (io/reader "resources/words.txt")]
     (doall (line-seq reader))))
-
-(defn transpose [m]
-  (apply mapv vector m))
-
-(defn fold-guesses
-  "returns for every letter position a map with keys :word/miss, :word/contains,
-  :word/match and as vals a set of the letters"
-  [guesses]
-  (->> guesses
-       (transpose)
-       (mapv (fn [xs] (group-by first xs)))
-       (mapv (fn [xs]
-              (reduce-kv (fn [m k v]
-                           (update m k (fnil into #{}) (map second v))) {} xs)))))
 
 (defn build-index
   "returns an index:
@@ -70,47 +56,93 @@
   [words]
   (build-index-per-letter words remove))
 
-(def ^:dynamic *db* (build-index words))
-(def ^:dynamic *db2* (build-contains-index words))
-(def ^:dynamic *db3* (build-without-index words))
+(defprotocol WordDB
+  (match [this length pos letter] "returns all words, of the given length,
+                              that match the letter at the given position")
+  (contains [this length letter] "returns all words, that contain the given letter")
+  (without [this length letter] "returns all words without the given letter"))
+
+(deftype Words [idx1 idx2 idx3]
+  WordDB
+  (match [this length pos letter] (get-in idx1 [length pos letter]))
+  (contains [this length letter] (get-in idx2 [length letter]))
+  (without [this length letter] (get-in idx3 [length letter])))
+
+(def DB (Words. (build-index words)
+                (build-contains-index words)
+                (build-without-index words)))
+
+
+;; CORE LOGIC RULES
 
 (defn ismatch
-    [w wlc pos letter]
+    [w length pos letter]
     (fn [env]
       (let [w (walk env w)]
         (if (lvar? w)
           (to-stream
-           (for [word (get-in *db* [wlc pos letter])]
+           (for [word (match DB length pos letter)]
              (unify env w word)))
           env))))
 
 (defn containo
-    [w wlc pos letter]
+    [w length pos letter]
     (fn [env]
       (let [w (walk env w)]
         (if (lvar? w)
           (to-stream
            (for [word (difference
-                       (get-in *db2* [wlc letter])
-                       (get-in *db* [wlc pos letter]))]
+                       (contains DB length letter)
+                       (match DB length pos letter))]
              (unify env w word)))
           env))))
 
-(defn missingo [w wlc letters]
+(defn missingo [w length letters]
     (fn [env]
       (let [w (walk env w)]
         (if (lvar? w)
           (to-stream
            (for [word (apply intersection
-                             (map #(get-in *db3* [wlc %]) letters))]
+                             (map #(without DB length %) letters))]
              (unify env w word)))
           env))))
+
+
+(defn transpose [m]
+  (apply mapv vector m))
+
+(defn fold-guesses
+  "returns for every letter position a map with keys :word/miss, :word/contains,
+  :word/match and as vals a set of the letters"
+  [guesses]
+  (->> guesses
+       (transpose)
+       (mapv (fn [xs] (group-by first xs)))
+       (mapv (fn [xs]
+              (reduce-kv (fn [m k v]
+                           (update m k (fnil into #{}) (map second v))) {} xs)))))
+
+
+
+
+
 
 (comment
   (def guesses
     [[[:word/miss \s] [:word/miss \i] [:word/miss \c] [:word/miss \h]]
      [[:word/miss \a] [:word/miss \t] [:word/contains \e] [:word/miss \m]]
      [[:word/miss \d] [:word/match \e] [:word/match \n] [:word/match \n]]])
+
+  (def g (fold-guesses guesses))
+  (def length (count g))
+  (def match-or-contains (reduce (fn [miss elem] (union miss
+                                                      (:word/match elem)
+                                                      (:word/contains elem)))
+                          #{} g))
+  (def miss (difference
+             (reduce (fn [miss elem] (union miss (:word/miss elem))) #{} g)
+             match-or-contains))
+
 
 
   (require '[lhrb.words.core :as c])
@@ -132,10 +164,6 @@
       (containo w2 4 1 \c)
       (== w w1)
       (== w w2)))
-*db2*
-  (difference
-   (get-in db2 [\a])
-   (get-in *db* [0 \a]))
 
   (difference #{1 2} #{1 3})
 
